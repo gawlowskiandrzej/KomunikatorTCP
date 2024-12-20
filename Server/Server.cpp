05acca63fd148f12dedffb83067c25ba4e3d0150
inf155198 <andrzejoklol@gmail.com>
2024-12-20 14:23:47 +0100

    Basic server structure and setup vm with linux compilation

--- Server/Server.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-
-int main(int argc, char* argv[])
-{
-    printf("%s", "Hello World!\n");
-    return 0;
-}
---
+++ ServerLinux/ServerLinux.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <map>
+#include <cstring>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_address_len = sizeof(client_address);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        printf("Failed to create socket.\n");
+        return -1;
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        printf("Binding failed.\n");
+        close(server_socket);
+        return -1;
+    }
+
+    if (listen(server_socket, 10) == -1) {
+        printf("Listening failed.\n");
+        close(server_socket);
+        return -1;
+    }
+
+    printf("Server is running on port %d \n", PORT);
+
+    while (true) {
+        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
+        if (client_socket == -1) {
+            printf("Failed to accept client connection.\n");
+            continue;
+        }
+        if (client_socket > -1)
+        {
+            write(0,"Client connected!", 10);
+        }
+    }
+
+    close(server_socket);
+    return 0;
+}
